26e0fe41f9eb4163228d683c11f0c5af4948827b
tbsmith <tbsmith@ubuntu.(none)>
2012-10-06 19:01:02 -0400

    Made a new python script

--- hi.py
+++ hi.py
@@ -1,5 +1,21 @@
-#!/usr/bin/perl
+#!/usr/bin/python
 
-print "hi";
+msg = "This is a message"
+n = 19
 
-print "newf";
+print msg
+print n
+
+# Comment: This is my list
+
+myList = ['milk', 'eggs', 13, 21]
+
+print "There are", len(myList), "things on my list."
+print "The second item on the list is" , myList[1]
+
+i=0
+while i<10:
+	print i
+	i=i+1
+
+
